a597181edc0f5e89837595096a6e1fd9de61b5be
lillebitteditte <ditt2047@stud.kea.dk>
2024-12-16 17:08:02 +0100

    git commit -m "Changed folder structure"

--- funtions.php
+++ funtions.php
@@ -1,21 +1,39 @@
+<?php
+// Tilføj WooCommerce support
 function my_theme_add_woocommerce_support() {
-    add_theme_support( 'woocommerce' );
+    add_theme_support('woocommerce');
 }
-add_action( 'after_setup_theme', 'my_theme_add_woocommerce_support' );
+add_action('after_setup_theme', 'my_theme_add_woocommerce_support');
 
-
+// Enqueue styles og scripts
 function silverjournal_enqueue_scripts() {
-  // CSS
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap', [], null);
-  wp_enqueue_style('global-css', get_template_directory_uri() . '/global.css', [], '1.0.0');
-  wp_enqueue_style('homepage-css', get_template_directory_uri() . '/homepage.css', [], '1.0.0');
-  wp_enqueue_style('gallery-css', get_template_directory_uri() . '/gallery.css', [], '1.0.0');
-  wp_enqueue_style('about-css', get_template_directory_uri() . '/about.css', [], '1.0.0');
-  wp_enqueue_style('style-css', get_template_directory_uri() . '/style.css', [], '1.0.0');
+    // Global styles og scripts
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap', [], null, 'all');
+    wp_enqueue_style('global-css', get_template_directory_uri() . '/css/global.css', [], '1.0.0', 'all');
+    wp_enqueue_script('global-js', get_template_directory_uri() . '/js/global.js', [], '1.0.0', true);
 
-  // JavaScript
-  wp_enqueue_script('global-js', get_template_directory_uri() . '/global.js', [], '1.0.0', true);
-  wp_enqueue_script('gallery-js', get_template_directory_uri() . '/gallery.js', [], '1.0.0', true);
-  wp_enqueue_script('about-js', get_template_directory_uri() . '/about.js', [], '1.0.0', true);
+    // Side-specifik CSS og JS
+    if (is_front_page()) {
+        wp_enqueue_style('homepage-css', get_template_directory_uri() . '/css/homepage.css', [], '1.0.0', 'all');
+        wp_enqueue_script('homepage-js', get_template_directory_uri() . '/js/homepage.js', [], '1.0.0', true);
+    }
+
+    if (is_page_template('templates/gallery.php')) {
+        wp_enqueue_style('gallery-css', get_template_directory_uri() . '/css/gallery.css', [], '1.0.0', 'all');
+        wp_enqueue_script('gallery-js', get_template_directory_uri() . '/js/gallery.js', [], '1.0.0', true);
+    }
+
+    if (is_page_template('templates/about.php')) {
+        wp_enqueue_style('about-css', get_template_directory_uri() . '/css/about.css', [], '1.0.0', 'all');
+        wp_enqueue_script('about-js', get_template_directory_uri() . '/js/about.js', [], '1.0.0', true);
+    }
 }
+
 add_action('wp_enqueue_scripts', 'silverjournal_enqueue_scripts');
+
+// Fjern uønskede body-classes
+function remove_body_classes($classes) {
+    $classes = array_diff($classes, ['unnecessary-class1', 'unnecessary-class2']);
+    return $classes;
+}
+add_filter('body_class', 'remove_body_classes');
