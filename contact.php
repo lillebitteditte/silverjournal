80426cc25a811166aad71762f57ec689d6ca9c75
lillebitteditte <ditt2047@stud.kea.dk>
2025-01-08 12:02:58 +0100

    git commit -m "Contact page added"

--- contact.php
+++ contact.php
@@ -7,12 +7,33 @@ Template Name: contact-silverjournal
 <?php get_header(); ?>
 
 
-<h1>
-   contact
-    </h1>
-    
-    <p>
-      You're always welcome to write me an email at thesilverjournal@gmail.com or DM me on Instagram
-    </p>
+<div class="content-wrapper">
+        <div class="left-wrapper">
+            <div class="decorative-line"></div>
+            <div class="message">
+                <p>Let's create something beautiful together. I'm always open to new projects and collaborations.</p>
+            </div>
+            <div class="reach-out">
+                <p>thesilverjournal@gmail.com</p>
+            </div>
+        </div>
+
+        <div class="right-wrapper">
+            <div class="decorative-line"></div>
+            <div class="based">
+                <p id="based">BASED</p>
+                <p>copenhagen</p>
+            </div>
+            <div class="social">
+                <div class="some-logos">
+                    <img class="some-logo" src="https://cdn.glitch.global/77744936-3f7b-4f61-aaf7-f2fa1dbe3cc9/instagram-white-icon.png?v=1736257961763" alt="Instagram">
+                    <img class="some-logo" src="https://cdn.glitch.global/77744936-3f7b-4f61-aaf7-f2fa1dbe3cc9/youtube-app-white-icon.png?v=1736257967678" alt="YouTube">
+                </div>
+                <div class="some-handle">
+                    <p>@thesilverjournall</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
 <footer class="footer">
-      <div class="footer-info">
-        
+<div class="footer-info">
         <p class="footer-item" > Mathias Andreasen | Photographer and YouTube content creator | The Silver Journal</p>
-        <a class="footer-item" href="#">instagram</a>
-        <a class="footer-item" href="#">thesilverjournal@gmail.com</a>
-      
+        <a class="footer-item" href="https://www.instagram.com/thesilverjournall/">instagram</a>
+       
+        
+        <a class="footer-item" onclick="copyEmail()">thesilverjournal@gmail.com</a>
+        <a class="footer-item" href="https://www.youtube.com/@TheSilverJournall"></a>
       </div>
     </footer>
     <?php wp_footer(); ?>
