22536996ac4e149f99d3183d2f714ad3a878bd25
lillebitteditte <ditt2047@stud.kea.dk>
2025-01-10 08:11:27 +0100

    git commit -m “Small footer changes”

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 <footer class="footer">
 <div class="footer-info">
-        <p class="footer-item" > Mathias Andreasen | Photographer and YouTube content creator | The Silver Journal</p>
+        <p class="footer-item" > photographer and youtube content creator | </p>
+        <a class="footer-item" href="https://www.youtube.com/@TheSilverJournall">youtube</a>
         <a class="footer-item" href="https://www.instagram.com/thesilverjournall/">instagram</a>
-       
-        
         <a class="footer-item" onclick="copyEmail()">thesilverjournal@gmail.com</a>
-        <a class="footer-item" href="https://www.youtube.com/@TheSilverJournall"></a>
+        
       </div>
     </footer>
     <?php wp_footer(); ?>
