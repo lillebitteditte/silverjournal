dbd830d6a5b57251ed15a6eee4dccc1559353e0a
lillebitteditte <ditt2047@stud.kea.dk>
2025-03-18 14:51:37 +0100

    git commit -m “corrected safari code”

--- contact.php
+++ contact.php
@@ -39,8 +39,8 @@ Template Name: contact-silverjournal
             </div>
             <div class="social">
                 <div class="some-logos">
-                    <img class="some-logo" src="https://cdn.glitch.global/77744936-3f7b-4f61-aaf7-f2fa1dbe3cc9/instagram-white-icon.png?v=1736257961763" alt="Instagram">
-                    <img class="some-logo" src="https://cdn.glitch.global/77744936-3f7b-4f61-aaf7-f2fa1dbe3cc9/youtube-app-white-icon.png?v=1736257967678" alt="YouTube">
+                    <img class="some-logo" src="https://cdn.glitch.global/77744936-3f7b-4f61-aaf7-f2fa1dbe3cc9/instagram-white-icon.png?v=1736257961763" alt="Instagram" href="https://www.instagram.com/thesilverjournall/">
+                    <img class="some-logo" src="https://cdn.glitch.global/77744936-3f7b-4f61-aaf7-f2fa1dbe3cc9/youtube-app-white-icon.png?v=1736257967678" alt="YouTube" href="https://www.youtube.com/@TheSilverJournall">
                 </div>
                
             </div>
