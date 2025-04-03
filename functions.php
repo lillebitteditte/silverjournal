6815972294b1adb4cd256c0d0677098ed3b08521
lillebitteditte <ditt2047@stud.kea.dk>
2025-04-03 12:54:17 +0200

    git commit -m “functions changed”

--- functions.php
+++ functions.php
@@ -8,15 +8,14 @@ add_action('after_setup_theme', 'my_theme_add_woocommerce_support');
 // Enqueue styles og scripts
 function silverjournal_enqueue_scripts() {
     // Global styles og scripts
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap', [], null, 'all');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap', [], null, 'all');
     wp_enqueue_style('global-css', get_template_directory_uri() . '/css/global.css', [], '1.0.0', 'all');
     wp_enqueue_script('global-js', get_template_directory_uri() . '/js/global.js', [], '1.0.0', true);
 
     // Side-specifik CSS og JS
     if (is_front_page()) {
         wp_enqueue_style('homepage-css', get_template_directory_uri() . '/css/homepage.css', [], '1.0.0', 'all');
-        wp_enqueue_style('gallery-css', get_template_directory_uri() . '/css/gallery.css', [], '1.0.0', 'all');
-        wp_enqueue_script('homepage-js', get_template_directory_uri() . '/js/gallery.js', [], '1.0.0', true);
+        wp_enqueue_script('homepage-js', get_template_directory_uri() . '/js/homepage.js', [], '1.0.0', true);
     }
 
     if (is_page_template('gallery.php')) {
