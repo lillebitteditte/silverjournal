28dbba13a15ceda3f6b847dec6f744e75e87d884
lillebitteditte <ditt2047@stud.kea.dk>
2025-04-03 12:49:58 +0200

    git commit -m “new index”

--- index.php
+++ index.php
@@ -10,44 +10,52 @@ Template Name: homepage-silverjournal
 
 <main> 
    
-    <div class="carousel_wrapper">
-      <button class="arrow-left">
-        <svg 
-        width="53" 
-        height="16" 
-        viewbox="0 0 53 16" 
-        fill="none"
-        >
-          <path
-            d="M0.292893 7.29289C-0.0976311 7.68342 -0.097631 8.31658 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538407 7.04738 0.538407 6.65685 0.928932L0.292893 7.29289ZM1 9L53 9L53 7L1 7L1 9Z"
-            fill="#FAFAFA"
-          />
-        </svg>
-      </button>
+<div class="swipe-indicator">
+            <img src="https://cdn.glitch.global/ece17432-589a-42ec-b065-50b7c80bc0db/swipe-left.svg?v=1743497295684" alt="Swipe finger">
+           
+        </div>
 
-      <div class="carousel_container">
-        <div class="carousel" id="carousel-container"></div>
-      </div>
+        <div class="gallery">
+            <div class="gallery-slide active">
+                <img src="https://cdn.glitch.global/ece17432-589a-42ec-b065-50b7c80bc0db/English%20garden%20no%20border.jpg?v=1743498303873" alt="Gallery Image 1">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Directions_1.jpg?v=1737120042533" alt="Gallery Image 2">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Munich_1.jpg?v=1737120073617" alt="Gallery Image 3">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/47859fd9-ba01-4974-8569-e34829bff88a/DSC05493-Edit_1.jpg?v=1738052321128" alt="Gallery Image 4">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Directions_1.jpg?v=1737120042533" alt="Gallery Image 5">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/47859fd9-ba01-4974-8569-e34829bff88a/DSC05493-Edit_1.jpg?v=1738052321128" alt="Gallery Image 6">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Munich_1.jpg?v=1737120073617" alt="Gallery Image 7">
+            </div>
+            <div class="gallery-slide">
+                <img src="https://cdn.glitch.global/47859fd9-ba01-4974-8569-e34829bff88a/DSC05493-Edit_1.jpg?v=1738052321128" alt="Gallery Image 8">
+            </div>
 
-      <button class="arrow-right">
-        <svg
-          width="53"
-          height="16"
-          viewbox="0 0 53 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M52.7071 8.7071C53.0976 8.31658 53.0976 7.68341 52.7071 7.29289L46.3431 0.928928C45.9526 0.538404 45.3195 0.538404 44.9289 0.928928C44.5384 1.31945 44.5384 1.95262 44.9289 2.34314L50.5858 8L44.9289 13.6569C44.5384 14.0474 44.5384 14.6805 44.9289 15.0711C45.3195 15.4616 45.9526 15.4616 46.3431 15.0711L52.7071 8.7071ZM8.74228e-08 9L52 9L52 7L-8.74228e-08 7L8.74228e-08 9Z"
-            fill="#FAFAFA"
-          />
-        </svg>
-      </button>
-    </div>
-
+            <div class="gallery-navigation">
+                <div class="gallery-dot active" data-slide="0"></div>
+                <div class="gallery-dot" data-slide="1"></div>
+                <div class="gallery-dot" data-slide="2"></div>
+                <div class="gallery-dot" data-slide="3"></div>
+                <div class="gallery-dot" data-slide="4"></div>
+                <div class="gallery-dot" data-slide="5"></div>
+                <div class="gallery-dot" data-slide="6"></div>
+                <div class="gallery-dot" data-slide="7"></div>
+            </div>
+        </div>
+       
+       
     <!--- MODAL FOR CAROUSEL IMAGES --->
 
-    
     <div id="carousel-modal" class="hide">
       <div class="modal-content">
         <button id="carousel-modal-close">&times;</button>
@@ -61,22 +69,6 @@ Template Name: homepage-silverjournal
       </div>
     </div>
 
-
-    <div class="grid-container">
-      <div class="grid-gallery">
-        <div class="column">
-         
-            <div id="dynamic-grid" class="grid-gallery"></div>
-         
-        </div>
-      </div>
-    </div>
-
-    <div id="modal-container" class="modal hide">
-      <div class="modal-image-container">
-        <span id="modal-close">&times;</span>
-        <img id="modal-image" class="modal-content" />
-      </div>
     </div>
 
 </main>
