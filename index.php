6627f74101c638b9dd8f9bfc50a4c1ba722612f4
lillebitteditte <ditt2047@stud.kea.dk>
2025-04-17 19:51:45 +0200

    git commit -m “Hero pictures”

--- index.php
+++ index.php
@@ -15,29 +15,24 @@ Template Name: homepage-silverjournal
 
         <div class="gallery">
             <div class="gallery-slide active">
-                <img src="https://cdn.glitch.global/ece17432-589a-42ec-b065-50b7c80bc0db/English%20garden%20no%20border.jpg?v=1743498303873" alt="Gallery Image 1">
+                <img src="https://thesilverjournal.com/wp-content/uploads/2025/04/English-garden-no-border-scaled.jpg" alt="Gallery Image 1">
             </div>
             <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Directions_1.jpg?v=1737120042533" alt="Gallery Image 2">
+                <img src="https://thesilverjournal.com/wp-content/uploads/2025/04/eisenbach-surf-no-border-scaled.jpg" alt="Gallery Image 2">
             </div>
             <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Munich_1.jpg?v=1737120073617" alt="Gallery Image 3">
+                <img src="https://thesilverjournal.com/wp-content/uploads/2025/04/placeholderCar.png" alt="Gallery Image 3">
             </div>
             <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/47859fd9-ba01-4974-8569-e34829bff88a/DSC05493-Edit_1.jpg?v=1738052321128" alt="Gallery Image 4">
+                <img src="https://thesilverjournal.com/wp-content/uploads/2025/04/Early-morning-ferry-Kalundborg-no-border-scaled.jpg" alt="Gallery Image 4">
             </div>
             <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Directions_1.jpg?v=1737120042533" alt="Gallery Image 5">
+                <img src="https://thesilverjournal.com/wp-content/uploads/2025/04/Autobahn-outback-scaled.jpg" alt="Gallery Image 5">
             </div>
             <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/47859fd9-ba01-4974-8569-e34829bff88a/DSC05493-Edit_1.jpg?v=1738052321128" alt="Gallery Image 6">
+                <img src="https://thesilverjournal.com/wp-content/uploads/2025/04/Hellerup-no-border-scaled.jpg" alt="Gallery Image 6">
             </div>
-            <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/977d6639-74aa-4742-8855-39891de14bf9/Munich_1.jpg?v=1737120073617" alt="Gallery Image 7">
-            </div>
-            <div class="gallery-slide">
-                <img src="https://cdn.glitch.global/47859fd9-ba01-4974-8569-e34829bff88a/DSC05493-Edit_1.jpg?v=1738052321128" alt="Gallery Image 8">
-            </div>
+           
 
             <div class="gallery-navigation">
                 <div class="gallery-dot active" data-slide="0"></div>
